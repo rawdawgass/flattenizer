4e100d5c68836951318ec8670525bfe85eab55c8
Andy Do <andydinhdo@gmail.com>
2016-02-18 02:38:18 -0800

    flattened CO successfully to local folder, now to send the flattened file back....

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import os
-# We'll render HTML templates and access data sent by POST
-# using the request object from flask. Redirect and url_for
-# will be used to redirect the user once the upload is done
-# and send_from_directory will help us to send/show on the
-# browser the file that the user just uploaded
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
+from flattenizer import flattenizer
+import pandas as pd
+from os.path import join
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-# These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['xlsx'])
 
 # For a given file, return whether it's an allowed type or not
@@ -27,22 +23,18 @@ def allowed_file(filename):
 def index():
     return render_template('flattenizer.html')
 
-
 # Route that will process the file upload
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get the name of the uploaded file
     file = request.files['file']
-    # Check if the file is one of the allowed types/extensions
     if file and allowed_file(file.filename):
-        # Make the filename safe, remove unsupported chars
         filename = secure_filename(file.filename)
-        # Move the file form the temporal folder to
-        # the upload folder we setup
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # Redirect the user to the uploaded_file route, which
-        # will basicaly show on the browser the uploaded file
-        return 'File uploaded'
+        
+
+
+        flattenizer(join(app.config['UPLOAD_FOLDER'], filename), join(app.config['UPLOAD_FOLDER']))
+        #file.save(join(app.config['UPLOAD_FOLDER'], filename))
+        return "File flattenized!!!"
     else:
         return render_template('nofile.html')
 
